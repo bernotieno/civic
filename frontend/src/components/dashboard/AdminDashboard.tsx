138becf9f9f408b7bbb662bafd553f4cc6528b2b
bernotieno <bernadokumu1@gmail.com>
2025-09-04 11:52:53 +0300

    removed the tempered frontend

--- frontend/src/components/dashboard/AdminDashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminOverview from './AdminOverview';
-
-const AdminDashboard: React.FC = () => {
-  return <AdminOverview />;
-};
-
-export default AdminDashboard;
